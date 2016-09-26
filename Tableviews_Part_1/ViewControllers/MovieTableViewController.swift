8d49ebebcbbf07c6bef3c4365ba39cd19dea472e
edwardanchundia <edward.anchundia@gmail.com>
2016-09-26 01:54:16 -0400

    unit 2 weekend hw tableviews

--- Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
     
+    enum Genre: Int {
+        case animation
+        case action
+        case drama
+        case twentiethCentury
+        case twentyFirstCentury
+    }
+    
     internal var movieData: [Movie]?
 
     internal let rawMovieData: [[String : Any]] = movies
@@ -34,21 +42,78 @@ class MovieTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieData?.count ?? 0
+        guard let genre = Genre.init(rawValue: section), let data = byGenre(genre)
+            else {
+                return 0
+        }
+        
+        return data.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        guard  let data = movieData else {
-            return cell
+        
+        guard let genre = Genre.init(rawValue: indexPath.section), let data = byGenre(genre)
+            else {
+                return cell
         }
-        cell.textLabel?.text = movieData?[indexPath.row].title
+        
+        cell.textLabel?.text = data[indexPath.row].title
         cell.detailTextLabel?.text = String(data[indexPath.row].year)
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let genre = Genre.init(rawValue: section) else { return "" }
+        
+        switch genre {
+        case .action:
+            return "Action"
+        case .animation:
+            return "Animation"
+        case .drama:
+            return "Drama"
+        case.twentiethCentury:
+            return "20th Century"
+        case.twentyFirstCentury:
+            return "21st Century"
+        }
+    }
+    
+    func byGenre(_ genre: Genre) -> [Movie]? {
+        let filter: (Movie) -> Bool
+        switch genre {
+        case .action:
+            filter = { (a) -> Bool in
+                a.genre == "action"
+            }
+        case .animation:
+            filter = { (a) -> Bool in
+                a.genre == "animation"
+            }
+        case .drama:
+            filter = { (a) -> Bool in
+                a.genre == "drama"
+            }
+        case .twentiethCentury:
+            filter = { (a) -> Bool in
+                a.year < 2000
+            }
+        case .twentyFirstCentury:
+            filter = { (a) -> Bool in
+                a.year >= 2000
+            }
+            
+        }
+        
+        // after filtering, sort
+        let filtered = movieData?.filter(filter).sorted {  $0.year < $1.year }
+        
+        return filtered
     }
 }
